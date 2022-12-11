e5c29914a942dafcfe7cd803e8ad8ab676a745ce
yarlagaddajeevika <47218938+yarlagaddajeevika@users.noreply.github.com>
2022-12-10 22:08:19 -0800

    Update cipher.cpp

--- cipher.cpp
+++ cipher.cpp
@@ -7,6 +7,15 @@
 
 using namespace std;
 
+#include <string>
+#include "CipherInterface.h"
+#include "DES.h"
+#include "AES.h"
+#include<iostream>
+#include <fstream> 
+
+using namespace std;
+
 int main(int argc, char** argv)
 {
 	/**
@@ -17,8 +26,6 @@ int main(int argc, char** argv)
 	 * NOTE: due to the incomplete skeleton, the code may crash or
 	 * misbehave.
 	 */
-	
-	
 	
 	/* Create an instance of the DES cipher */	
 	CipherInterface* cipher = NULL; 
@@ -31,106 +38,115 @@ int main(int argc, char** argv)
 	 */
    string cipherName = argv[1];
    char* key = argv[2];
+   char* temp = 0;
    string encFlag = argv[3];
    string inFile = argv[4];
    string outFile = argv[5];
-   std::cout << inFile <<endl;
-   std::cout << outFile << endl;
-   std::cout << encFlag << endl;
-   string final="";
+
     if(cipherName == "DES")
     {
       cipher = new DES();
 
       cipher->setKey((unsigned char*)key);
-      ofstream MyFile;
+      
       if(encFlag=="ENC"){
-		string s;
-		unsigned char* newc;
-		// Read from the text file
+		
 		ifstream MyReadFile(inFile);
+		ofstream MyFile(outFile);
+		unsigned char* newc;
+		string s;
 
-		// Use a while loop together with the getline() function to read the file line by line
-		while (getline (MyReadFile, s)) {
-		// Output the text from the file
-			// cout << s;
-			int n = s.length();
-		
-			// declaring character array
-			char char_array[n + 1];
-		
-			// copying the contents of the
-			// string to char array
-			strcpy(char_array, s.c_str());
+		if(MyReadFile && MyFile){
+			while (getline (MyReadFile, s)) {
+				int n = s.length();
+				char array[n + 1];
+				strcpy(array, s.c_str());
 
-			newc = cipher->encrypt((unsigned char*)char_array);
-			
-			// final+=(string)newc;
-			// cout << char_array;
-		
-			//TODO Write to a file
-			// Create and open a text file
-			// ofstream outfile;
-			MyFile.open("temp.txt", ios::app);
-
-			// // Write to the file
-			// cout << newc;
-			MyFile << newc;
-			cout << endl << "Printed" << endl;
-
-			// Close the file
-			
+				newc = cipher->encrypt((unsigned char*)array);
+				
+				MyFile << newc;
+				MyFile << "\n";
+			}
+			MyFile.close();
+			MyReadFile.close();
 		}
-		// newc = cipher->encrypt((unsigned char*)char_array);
-		// Close the file
-		MyReadFile.close(); 
-		MyFile.close();
-        
       }else{
+
+		ifstream MyReadFile(inFile);
+		ofstream MyFile(outFile);
 		string s;
 		unsigned char* newc;
 
-		ifstream MyReadFile(inFile);
+		if(MyReadFile && MyFile){
+			while (getline (MyReadFile, s)) {
+				int n = s.length();
+				char array[n + 1];
+				strcpy(array, s.c_str());
+				// cout << array << endl;
+				newc = cipher->decrypt((unsigned char*)array);
 
-		// Use a while loop together with the getline() function to read the file line by line
-		while (getline (MyReadFile, s)) {
-		// Output the text from the file
-			// cout << s;
-			int n = s.length();
-		
-			// declaring character array
-			char char_array[n + 1];
-		
-			// copying the contents of the
-			// string to char array
-			strcpy(char_array, s.c_str());
-
-			newc = cipher->decrypt((unsigned char*)char_array);
-			
-			
+				// cout << newc;
+				MyFile << newc;
+				MyFile << "\n";
+			}
+			MyFile.close();
+			MyReadFile.close();
 		}
-		cout << newc;
-		// MyReadFile.close(); 
-
-		// //TODO Write to a file
-		// // Create and open a text file
-		// ofstream MyFile(outFile);
-
-		// // Write to the file
-		
-		// MyFile << newc;
-
-		// // Close the file
-		// MyFile.close();
-		
 	  }
     }else{
       cipher = new AES();
-	  std::cout << "DEcryption" << endl;
-      cipher->setKey((unsigned char*)"0123456789abcdef");
+	        
 	  if(encFlag=="ENC"){
-		}else{
+		char a[3] = "00";
+		
+		strcat(key,a);
+    
+		cipher->setKey((unsigned char*)key);
+		ifstream MyReadFile(inFile);
+		ofstream MyFile(outFile);
+		unsigned char* newc;
+		string s;
+
+		if(MyReadFile && MyFile){
+			while (getline (MyReadFile, s)) {
+				int n = s.length();
+				char array[n + 1];
+				strcpy(array, s.c_str());
+
+				newc = cipher->encrypt((unsigned char*)array);
+				
+				MyFile << newc;
+				MyFile << "\n";
+			}
+			MyFile.close();
+			MyReadFile.close();
 		}
+	  }else{
+		char a[3] = "01";
+		
+		strcat(key,a);
+		
+		cipher->setKey((unsigned char*)key);
+		ifstream MyReadFile(inFile);
+		ofstream MyFile(outFile);
+		string s;
+		unsigned char* newc;
+
+		if(MyReadFile && MyFile){
+			while (getline (MyReadFile, s)) {
+				int n = s.length();
+				char array[n + 1];
+				strcpy(array, s.c_str());
+				
+				newc = cipher->decrypt((unsigned char*)array);
+
+				MyFile << newc;
+				MyFile << "\n";
+			}
+			MyFile.close();
+			MyReadFile.close();
+		}
+	  }
     }
 	/* Error checks */
 	if(!cipher)
@@ -138,12 +154,7 @@ int main(int argc, char** argv)
 		fprintf(stderr, "ERROR [%s %s %d]: could not allocate memory\n",	
 		__FILE__, __FUNCTION__, __LINE__);
 		exit(-1);
-	}
-	/* Perform encryption */
-	// string cipherText = cipher->encrypt("hello world");
-	
-	/* Perform decryption */
-	//cipher->decrypt(cipherText);	
+	}	
 	
 	return 0;
 }
